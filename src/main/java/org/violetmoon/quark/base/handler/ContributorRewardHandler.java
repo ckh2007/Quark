b050fd933738103fbfeb4ab1d8a2d2bdd7e47e1e
ckh2007 <55984222+ckh2007@users.noreply.github.com>
2024-02-21 12:46:31 +0800

    Update ContributorRewardHandler.java

--- src/main/java/org/violetmoon/quark/base/handler/ContributorRewardHandler.java
+++ src/main/java/org/violetmoon/quark/base/handler/ContributorRewardHandler.java
@@ -20,7 +20,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
-import java.util.*;
+import java.util.*; 
 
 public class ContributorRewardHandler {
 
